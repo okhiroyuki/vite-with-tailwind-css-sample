a42e6ba8213c9d199158c9e8edc059126fbd62dd
Hiroyuki Okada <ok.okada.hiroyuki@gmail.com>
2024-04-06 12:37:18 +0000

    style: tab to space

--- vite.config.js
+++ vite.config.js
@@ -2,21 +2,21 @@ import { visualizer } from "rollup-plugin-visualizer";
 import { defineConfig } from "vite";
 
 export default defineConfig(({ mode }) => {
-	return {
-		base: "./",
-		build: {
-			outDir: "dist",
-			rollupOptions: {
-				plugins: [
-					mode === "analyze" &&
-						visualizer({
-							open: true,
-							filename: "stats.html",
-							gzipSize: true,
-							brotliSize: true,
-						}),
-				],
-			},
-		},
-	};
+  return {
+    base: "./",
+    build: {
+      outDir: "dist",
+      rollupOptions: {
+        plugins: [
+          mode === "analyze" &&
+            visualizer({
+              open: true,
+              filename: "stats.html",
+              gzipSize: true,
+              brotliSize: true,
+            }),
+        ],
+      },
+    },
+  };
 });
