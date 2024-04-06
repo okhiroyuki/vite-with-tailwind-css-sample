5cbad045276ec1fb1a16b2ef7245574937881f6f
Hiroyuki Okada <ok.okada.hiroyuki@gmail.com>
2024-04-06 21:33:59 +0900

    feat: add analyze scripts (#3)

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,22 @@
+import { visualizer } from "rollup-plugin-visualizer";
 import { defineConfig } from "vite";
 
-export default defineConfig({
-	base: "./",
+export default defineConfig(({ mode }) => {
+	return {
+		base: "./",
+		build: {
+			outDir: "dist",
+			rollupOptions: {
+				plugins: [
+					mode === "analyze" &&
+						visualizer({
+							open: true,
+							filename: "stats.html",
+							gzipSize: true,
+							brotliSize: true,
+						}),
+				],
+			},
+		},
+	};
 });
